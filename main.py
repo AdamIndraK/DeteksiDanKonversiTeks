66cdedfbcff7dd5e790556cf6af6fa63128b9755
AdamIndraK <mysteryhall3@gmail.com>
2023-12-11 16:14:25 +0800

    TambahanLagi

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ def detect_image():
         return jsonify({"result_image": "Belum Ada", "result_message": "Belum Ada"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
